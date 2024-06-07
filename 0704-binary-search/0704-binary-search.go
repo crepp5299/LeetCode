4e7d47e3e814f94788bd662c0f1382c0290ff3af
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-06-08 00:49:13 +0700

    Time: 24 ms (70.54%), Space: 6.8 MB (72.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.go
+++ 0704-binary-search/0704-binary-search.go
@@ -1,9 +1,6 @@
 func search(nums []int, target int) int {
     l, r := 0, len(nums) - 1
     for l <= r {
-        if l > r {
-            break
-        }
         m := (l+r)/2
         if nums[m] == target {
             return m
