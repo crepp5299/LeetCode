cd742f9dc4a72878f8ab5eb910943b34dcda6baa
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-07-03 09:49:14 +0700

    Time: 61 ms (76.67%), Space: 8.9 MB (16.67%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.go
@@ -0,0 +1,15 @@
+func minDifference(nums []int) int {
+    sort.Ints(nums)
+    l:= len(nums)
+    if l <= 4 {
+        return 0
+    }
+    t:=len(nums)-3-1
+    rs := nums[t] - nums[0]
+    for i:=1;i+t < l;i++ {
+        if nums[i+t] - nums[i] < rs {
+            rs = nums[i+t] - nums[i]
+        }
+    }
+    return rs
+}
