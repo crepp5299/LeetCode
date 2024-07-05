6dee429b7a7d39ba98ba5703dc57b6338a39c885
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-07-05 10:25:16 +0700

    Time: 155 ms (18.18%), Space: 10.5 MB (54.55%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.go
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.go
@@ -9,29 +9,28 @@ func nodesBetweenCriticalPoints(head *ListNode) []int {
     cur := head
     prevVal := head.Val
     cur = cur.Next
-    count := 0
-    arr := []int{}
+    count := 1
+    first, prev, min := 0, -1, -1
     for cur != nil && cur.Next != nil {
         if (cur.Val > prevVal && cur.Val > cur.Next.Val) ||  (cur.Val < prevVal && cur.Val < cur.Next.Val){
-            arr = append(arr, count)
+            if min == -1 && first != 0 {
+                min = count - prev
+            }
+            if count - prev < min {
+                min = count - prev
+            }
+            if prev == -1 {
+                first = count
+            }
+            prev = count
         }
         prevVal = cur.Val
         cur = cur.Next
         count++
     }
-    l := len(arr)
-    if l < 2 {
+    if prev == first {
         return []int{-1,-1}
     }
-    result := make([]int, 2)
-    result[0] = arr[1] - arr[0]
-    result[1] = 0
-    for i := 1; i< l; i++ {
-        if arr[i] - arr[i-1] < result[0] {
-            result[0] = arr[i] - arr[i-1]
-        }
-    }
-    result[1] = arr[l-1] - arr[0]
-    
-    return result
+    return []int{min, prev-first}
+
 }
