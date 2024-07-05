3fc2ce0dc276ffff1271062420c26fffed8614f0
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-07-05 10:30:29 +0700

    Time: 143 ms (27.27%), Space: 8.3 MB (90.91%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.go
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.go
@@ -10,25 +10,25 @@ func nodesBetweenCriticalPoints(head *ListNode) []int {
     prevVal := head.Val
     cur = cur.Next
     count := 1
-    first, prev, min := 0, -1, -1
+    first, prev, min := 0, 0, 999999 
     for cur != nil && cur.Next != nil {
         if (cur.Val > prevVal && cur.Val > cur.Next.Val) ||  (cur.Val < prevVal && cur.Val < cur.Next.Val){
-            if min == -1 && first != 0 {
-                min = count - prev
-            }
-            if count - prev < min {
-                min = count - prev
-            }
-            if prev == -1 {
+            if prev == 0 {
                 first = count
+                prev = count
+                continue
             }
+            if count - prev < min && count != prev{
+                min = count - prev
+            }
+           
             prev = count
         }
         prevVal = cur.Val
         cur = cur.Next
         count++
     }
-    if prev == first {
+    if prev == first || first == 0{
         return []int{-1,-1}
     }
     return []int{min, prev-first}
