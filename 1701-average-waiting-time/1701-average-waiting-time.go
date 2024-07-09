d5b57f40bd3643f26fd367fe20e34cbab3923059
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-07-09 09:27:22 +0700

    Time: 104 ms (83.33%), Space: 14.7 MB (75.00%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.go
+++ 1701-average-waiting-time/1701-average-waiting-time.go
@@ -3,15 +3,13 @@ func averageWaitingTime(customers [][]int) float64 {
     lastFinishTime := customers[0][1] + customers[0][0]
     l := len(customers)
     for i := 1; i < l; i++ {
-        fmt.Println(lastFinishTime,totalWait)
         if lastFinishTime >= customers[i][0] {
             totalWait += lastFinishTime - customers[i][0] + customers[i][1]
             lastFinishTime += customers[i][1]
-        } else {
-            totalWait += customers[i][1]
-            lastFinishTime = customers[i][1] + customers[i][0]
-        }
+            continue
+        } 
+        totalWait += customers[i][1]
+        lastFinishTime = customers[i][1] + customers[i][0]
     }
-    fmt.Println(lastFinishTime,totalWait)
     return float64(totalWait)/float64(l)
 }
