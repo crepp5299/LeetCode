4064731aa46d54178ff63eb6c207ea3219800bf3
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-07-25 08:47:13 +0700

    Time: 154 ms (52.81%), Space: 24.8 MB (10.19%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.go
+++ 0912-sort-an-array/0912-sort-an-array.go
@@ -1,4 +1,40 @@
-func sortArray(nums []int) []int {
-    sort.Ints(nums)
-    return nums
+
+
+func sortArray(items []int) []int {
+
+    if len(items) < 2 {
+
+        return items
+
+    }
+
+    first := sortArray(items[:len(items)/2])
+
+    second := sortArray(items[len(items)/2:])
+
+    return merge(first, second)
+
+}
+
+func merge(a []int, b []int) []int {
+    final := []int{}
+    i := 0
+    j := 0
+    for i < len(a) && j < len(b) {
+        if a[i] < b[j] {
+            final = append(final, a[i])
+            i++
+        } else {
+            final = append(final, b[j])
+            j++
+        }
+    }
+    for ; i < len(a); i++ {
+        final = append(final, a[i])
+    }
+    for ; j < len(b); j++ {
+        final = append(final, b[j])
+    }
+    return final
+
 }
