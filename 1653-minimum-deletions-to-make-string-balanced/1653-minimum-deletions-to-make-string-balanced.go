07cdf08e95635a39435baa18f5882aa5c8d78bd9
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-07-30 22:18:13 +0700

    Time: 18 ms (100.00%), Space: 8 MB (22.22%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.go
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.go
@@ -1,42 +1,41 @@
-// func minimumDeletions(s string) int {
-//     runes := []rune(s)
-//     n := len(runes)
-//     countA := make([]int, n)
-//     count := 0
-//     for i := n - 1; i >= 0; i-- {
-//         countA[i] = count
-//         if runes[i] == 'a' {
-//             count++
-//         }
-//     }
-    
-//     count = 0
-    
-//     for i, _ := range runes {
-//         if countA[i] + count < n {
-//             n = countA[i] + count
-//         }
-//         if runes[i] == 'b' {
-//             count++
-//         }
-//     }
-//     return n
-// }
-
 func minimumDeletions(s string) int {
-    // from current index, calculate how many 'b' to delete on left and 'a' to delete on right.
-    //in one pass get the acounts and bcounts
-    
-    acount, bcount := 0,0
-    acount = strings.Count(s,"a")
-    min:=acount
-    for _, v := range s {
-        if v == 'b' {
-            bcount++
-        } else {
-            acount--
+    n := len(s)
+    countA := make([]int, n)
+    count := 0
+    for i := n - 1; i >= 0; i-- {
+        countA[i] = count
+        if s[i] == 'a' {
+            count++
         }
-        min = int(math.Min(float64(min), float64(acount+bcount)))
     }
-    return min
+    
+    count = 0
+    
+    for i, _ := range s {
+        if countA[i] + count < n {
+            n = countA[i] + count
+        }
+        if s[i] == 'b' {
+            count++
+        }
+    }
+    return n
 }
+
+// func minimumDeletions(s string) int {
+//     // from current index, calculate how many 'b' to delete on left and 'a' to delete on right.
+//     //in one pass get the acounts and bcounts
+    
+//     acount, bcount := 0,0
+//     acount = strings.Count(s,"a")
+//     min:=acount
+//     for _, v := range s {
+//         if v == 'b' {
+//             bcount++
+//         } else {
+//             acount--
+//         }
+//         min = int(math.Min(float64(min), float64(acount+bcount)))
+//     }
+//     return min
+// }
