8819ff24d6142daf49619f39ba93181f6038e3ac
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-08-02 12:32:22 +0700

    Time: 82 ms (87.50%), Space: 10.2 MB (12.50%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.go
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.go
@@ -1,7 +1,5 @@
 func minSwaps(nums []int) int {
     n := len(nums)
-    nums2 := make([]int, 0, n)
-    nums2 = append(nums2, nums...)
     len1 := 0
     for i := range nums {
         if nums[i] == 1 {
@@ -16,9 +14,9 @@ func minSwaps(nums []int) int {
     }
     min := count
     for i := 0; i < n; i++ {
-        if nums2[(i+len1)%n] == 1 && nums2[i] == 0 {
+        if nums[(i+len1)%n] == 1 && nums[i] == 0 {
             count--
-        } else if nums2[(i+len1)%n] == 0 && nums2[i] == 1 {
+        } else if nums[(i+len1)%n] == 0 && nums[i] == 1 {
             count++
         }
         if min > count {
