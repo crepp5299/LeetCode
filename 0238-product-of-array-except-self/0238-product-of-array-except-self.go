89c53ef3f213e858821cabcd89418aebbdbfcea9
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-08-27 07:09:29 +0700

    Time: 46 ms (24.95%), Space: 7.7 MB (88.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,20 +1,16 @@
 func productExceptSelf(nums []int) []int {
-    i, j := 1, 1
     n := len(nums)
-    rs := make([]int, n)
-    for k:=0;k<n;k++{
-        if rs[k] == 0 && k < n/2{
-            rs[k] = i
-            rs[n-1-k] = j
-        } else {
-            rs[k]*=i
-            rs[n-1-k]*=j
-        }
-        if k == n-1-k {
-            rs[k] = i*j
-        }
-        i = i*nums[k]
-        j = j*nums[n-1-k]
+    result := make([]int, n)
+    x := 1
+    for i := range nums {
+        result[i] = x
+        x *= nums[i]
     }
-    return rs
+    x = 1
+    for n > 0 {
+        result[n-1] = x*result[n-1]
+        x *= nums[n-1]
+        n--
+    }
+    return result
 }
