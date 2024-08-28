f04450d35de5be3181741b9784321bfbb2b627b5
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-08-28 09:51:52 +0700

    Time: 18 ms (36.41%), Space: 6.9 MB (11.81%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -0,0 +1,18 @@
+func merge(intervals [][]int) [][]int {
+    sort.Slice(intervals[:], func(i, j int) bool {
+        return intervals[i][0] < intervals[j][0]
+    })
+    n:= len(intervals)
+    result := make([][]int, 0, n)
+    tempItem := []int{intervals[0][0], intervals[0][1]}
+    for i := 1; i < n; i++ {
+        if tempItem[1] < intervals[i][0] {
+            result = append(result, tempItem)
+            tempItem = intervals[i]
+        } else if tempItem[1] >= intervals[i][0] && tempItem[1] <= intervals[i][1]{
+            tempItem[1] = intervals[i][1]
+        }
+    }
+    result = append(result, tempItem)
+    return result
+}
