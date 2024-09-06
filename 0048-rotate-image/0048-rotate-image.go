e386765863ff024c3273e9367c25247c54375c08
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-06 09:53:36 +0700

    Time: 0 ms (100.00%), Space: 2.5 MB (57.07%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,18 +1,13 @@
 func rotate(matrix [][]int)  {
     n := len(matrix)
-    curI := 0
-    count := 0
-    for curI<=n/2 {
+    for curI := 0; curI<=n/2; curI++ {
         for curJ := curI; curJ < n-1-curI; curJ++{
             tempI, tempJ := curI, curJ
             tempVal := matrix[tempI][tempJ]
-            for count < 4 {
+            for count:=0; count < 4;count++ {
                 tempI, tempJ = tempJ, n-1-tempI
                 tempVal, matrix[tempI][tempJ] = matrix[tempI][tempJ], tempVal
-                count++
             }
-            count = 0
         }
-        curI++
     }
 }
