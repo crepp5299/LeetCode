39a9555f83fad77201486c3aa1311a50b690b325
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-09 10:07:41 +0700

    Time: 4 ms (92.29%), Space: 4 MB (81.10%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.go
+++ 0383-ransom-note/0383-ransom-note.go
@@ -1,14 +1,14 @@
 func canConstruct(ransomNote string, magazine string) bool {
-    m := make(map[byte]int, 26)
-    for i := range magazine {
-        m[magazine[i]]++
-    }
-    for i := range ransomNote {
-        if m[ransomNote[i]] > 0 {
-            m[ransomNote[i]]--
-        } else {
-            return false
-        }
-    }
-    return true
+	h := make([]int, 26)
+	for i := 0; i < len(magazine); i++ {
+		h[magazine[i]-97]++
+	}
+
+	for i := 0; i < len(ransomNote); i++ {
+		if h[ransomNote[i]-97] <= 0 {
+			return false
+		}
+		h[ransomNote[i]-97]--
+	}
+	return true
 }
