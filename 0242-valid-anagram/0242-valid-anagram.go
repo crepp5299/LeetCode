f54543627d7382735c5ca2727e4f702658dbfe61
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-10 09:10:20 +0700

    Time: 7 ms (55.40%), Space: 4.1 MB (9.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,16 +1,26 @@
+// func isAnagram(s string, t string) bool {
+//     if len(s) != len(t) {
+//         return false
+//     }
+//     m := make(map[byte]int, 26)
+//     for i := 0; i < len(s); i++ {
+//         m[s[i]]++
+//     }
+//     for i := 0; i < len(t); i++ {
+//         if m[t[i]] == 0 {
+//             return false
+//         }
+//         m[t[i]]--
+//     }
+//     return true
+// }
+
 func isAnagram(s string, t string) bool {
-    if len(s) != len(t) {
-        return false
-    }
-    m := make(map[byte]int, 26)
-    for i := 0; i < len(s); i++ {
-        m[s[i]]++
-    }
-    for i := 0; i < len(t); i++ {
-        if m[t[i]] == 0 {
-            return false
-        }
-        m[t[i]]--
-    }
-    return true
+    s1 := []rune(s)
+    t1 := []rune(t)
+    sort.Slice(s1, func(i int, j int) bool { return s1[i] < s1[j] })
+    sort.Slice(t1, func(i int, j int) bool { return t1[i] < t1[j] })
+    return string(s1) == string(t1)
 }
+
+    
