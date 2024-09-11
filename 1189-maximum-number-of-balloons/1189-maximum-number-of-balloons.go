5dafb766430fe7ed5f4e1b1d708f8da87e092490
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-11 08:50:25 +0700

    Time: 2 ms (62.86%), Space: 2.4 MB (24.29%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.go
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.go
@@ -1,6 +1,6 @@
 func maxNumberOfBalloons(text string) int {
     // b := map[rune]int{'b': 1, 'a' : 1, 'l' : 2, 'o' : 2, 'n':1 }
-    m := make(map[rune]int)
+    m := make(map[rune]int, 26)
     for _, c := range text {
         m[c]++
     }
