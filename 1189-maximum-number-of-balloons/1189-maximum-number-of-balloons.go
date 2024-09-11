480066e12438ae2874a627e7c70aef1ace33ec13
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-11 08:50:58 +0700

    Time: 2 ms (62.86%), Space: 2.4 MB (97.14%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.go
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.go
@@ -1,26 +1,14 @@
 func maxNumberOfBalloons(text string) int {
-    // b := map[rune]int{'b': 1, 'a' : 1, 'l' : 2, 'o' : 2, 'n':1 }
+    b := map[rune]int{'b': 1, 'a' : 1, 'l' : 2, 'o' : 2, 'n':1 }
     m := make(map[rune]int, 26)
     for _, c := range text {
         m[c]++
     }
     min := math.MaxInt
-    min = getMin(m['b'], min)
-    min = getMin(m['a'], min)
-    min = getMin(m['l']/2, min)
-    min = getMin(m['o']/2, min)
-    min = getMin(m['n'], min)
-    // for k, v := range b {
-    //     if m[k]/v < min {
-    //         min = m[k]/v
-    //     }
-    // }
-    return min
-}
-
-func getMin(a, b int) int {
-    if a > b {
-        return b
+    for k, v := range b {
+        if m[k]/v < min {
+            min = m[k]/v
+        }
     }
-    return a
+    return min
 }
