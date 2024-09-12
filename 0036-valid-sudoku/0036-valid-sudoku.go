d2b5c746dc8a69709d298577285c80b8c64e9717
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-12 15:52:34 +0700

    Time: 5 ms (27.16%), Space: 2.8 MB (84.11%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.go
@@ -0,0 +1,41 @@
+func isValidSudoku(board [][]byte) bool {
+    for i := 0; i < 9; i++ {
+        m := make(map[byte]bool)
+        n := make(map[byte]bool)
+        for j := 0; j < 9; j++ {
+            if m[board[i][j]] {
+                return false
+            }
+            if board[i][j] != '.' {
+                m[board[i][j]] = true
+            }
+            if n[board[j][i]] {
+                return false
+            }
+            if board[j][i] != '.' {
+                n[board[j][i]] = true
+            }
+        }
+    }
+    
+    items := [][]int{
+        {0, 0}, {0, 3}, {0, 6},
+        {3, 0}, {3, 3}, {3, 6},
+        {6, 0}, {6, 3}, {6, 6},
+    }
+    for index := range items {
+        m := make(map[byte]bool)
+        for i := 0; i < 3; i++ {
+            for j := 0; j < 3; j++ {
+                if m[board[items[index][0]+i][items[index][1]+j]] {
+                return false
+            }
+            if board[items[index][0]+i][items[index][1]+j] != '.' {
+                m[board[items[index][0]+i][items[index][1]+j]] = true
+            }
+        }
+        }
+    }
+    
+    return true
+}
