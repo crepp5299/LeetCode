3cf727bef41390cce1d57ed251e4934a1acad7b5
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-13 10:05:33 +0700

    Time: 20 ms (51.60%), Space: 6.5 MB (80.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,10 +1,11 @@
 func majorityElement(nums []int) int {
     m := make(map[int]int)
-    for _, v := range nums{
-        m[v]++
+    n := len(nums)
+    for i := 0; i < n; i++ {
+        m[nums[i]]++
     }
     for k, v := range m {
-        if v > len(nums)/2 {
+        if v > n/2 {
             return k
         }
     }
