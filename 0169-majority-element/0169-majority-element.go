07d53ca005572a84a5210908aee756b46c98ddfd
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-13 11:53:23 +0700

    Time: 20 ms (51.60%), Space: 7 MB (18.30%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,13 +1,33 @@
+// func majorityElement(nums []int) int {
+//     m := make(map[int]int)
+//     n := len(nums)
+    
+//     for i := 0; i < n; i++ {
+//         m[nums[i]]++
+//     }
+//     for k, v := range m {
+//         if v > n/2 {
+//             return k
+//         }
+//     }
+//     return 0
+// }
+
 func majorityElement(nums []int) int {
-    m := make(map[int]int)
+    sort.Slice(nums, func(i, j int) bool {
+    return nums[i] < nums[j]
+    })
+    count := 1
     n := len(nums)
-    for i := 0; i < n; i++ {
-        m[nums[i]]++
-    }
-    for k, v := range m {
-        if v > n/2 {
-            return k
+    for i := 1; i < n; i++ {
+        if nums[i] == nums[i-1] {
+            count++
+        } else {
+            count = 1
+        }
+        if count > n/2 {
+            return nums[i]
         }
     }
-    return 0
+    return nums[0]
 }
