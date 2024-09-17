d4d0f66b6ee67a0abde927c3e88b28381e56c641
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-09-17 13:46:51 +0700

    Time: 28 ms (74.12%), Space: 6.5 MB (86.96%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.go
+++ 0344-reverse-string/0344-reverse-string.go
@@ -1,7 +1,8 @@
 func reverseString(s []byte)  {
-    i, n := 0, len(s)
-    for i < n/2 {
-        s[i], s[n-i-1] = s[n-i-1], s[i]
+    i, j := 0, len(s)-1
+    for i < j {
+        s[i], s[j] = s[j], s[i]
         i++
+        j--
     }
 }
