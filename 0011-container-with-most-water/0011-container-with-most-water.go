8fd434518be6b370c30e048184aa8bde747dcb8d
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-10-22 14:20:55 +0700

    Time: 0 ms (100.00%), Space: 9.2 MB (17.44%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.go
+++ 0011-container-with-most-water/0011-container-with-most-water.go
@@ -1,6 +1,6 @@
 func maxArea(height []int) int {
     i, j := 0, len(height) - 1
-    max := min(height[i], height[j]) * (j-i)
+    max := 0
     for i < j {
         if min(height[i], height[j]) * (j-i) > max {
             max = min(height[i], height[j]) * (j-i)
