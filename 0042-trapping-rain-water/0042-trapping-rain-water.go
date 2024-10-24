a51605253507e5fdd4ea5989304a21f1036496b9
Nguyễn Tấn Thái <44206665+crepp5299@users.noreply.github.com>
2024-10-24 15:31:50 +0700

    Time: 0 ms (100.00%), Space: 8 MB (5.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.go
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -1,17 +1,18 @@
 func trap(height []int) int {
     h := len(height)
-    l, maxL, maxR, rs := make([]int, h), 0, 0, 0
+    l, maxL, maxR := make([]int, h), 0, 0
     for i := range height {
         l[i] = maxL
         maxL = max(height[i], maxL)
     }
+    maxL = 0
     for i := range height {
         if min(l[h-1-i], maxR) - height[h-1-i] > 0 {
-            rs += min(l[h-1-i], maxR) - height[h-1-i]
+            maxL += min(l[h-1-i], maxR) - height[h-1-i]
         }
         maxR = max(height[h-1-i], maxR)
     }
-    return rs
+    return maxL
 }
 
 func max(a, b int) int {
